28228c01bf677efa2f810f4547684079543ad09a
Amilcov <Amilcov@yahoo.com>
2022-07-07 01:02:59 +0300

    Add files via upload

---
+++ Day2/3.1 App with Authentication- Etapa 1- Authentification and Authorization/db/models/user.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: {
+      allowNull: false,
+      type: DataTypes.STRING(50)
+    },
+    lastName: {
+      allowNull: false,
+      type: DataTypes.STRING(50)
+    },
+    emailAddress: {
+      allowNull: false,    
+      type: DataTypes.STRING(255),
+      unique: true
+    },
+    hashedPassword: {
+      allowNull: false,
+      type: DataTypes.STRING.BINARY
+    }
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
