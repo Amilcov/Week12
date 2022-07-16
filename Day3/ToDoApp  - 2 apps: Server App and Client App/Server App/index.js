7512186d3ee78af8fa1d1afba6b7c108bd22f4eb
Amilcov <Amilcov@yahoo.com>
2022-07-17 00:23:07 +0300

    Delete Day3/ToDoApp  - 2 apps: Server App and Client App/Server App directory

--- Day3/ToDoApp  - 2 apps: Server App and Client App/Server App/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.get('/', (req, res) => {
-    res.json({message: 'Hello from the Express APIs demo app!'});
-});
-
-module.exports = router;
