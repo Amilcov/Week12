d438c7d0f6173624f7c45ad1a4c6c81602e74035
Amilcov <Amilcov@yahoo.com>
2022-07-18 09:27:47 +0300

    Add files via upload

--- Day3/2. Twitter Lite/server/app.js
+++ Day3/2. Twitter Lite/server/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors');
+
 const { environment } = require('./config');
 const indexRouter = require('./routes/index');
 const tweetsRouter =  require('./routes/tweets');
+
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors());
+app.use(cors({"origin": "http://localhost:4000"}));
 
 app.use('/', indexRouter);
 app.use('/tweets', tweetsRouter)
